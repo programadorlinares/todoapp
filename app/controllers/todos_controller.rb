2c552405dfaa0f12e679c77edd34e56fadac1cb1
mauriciolarag <mauriciolarag@mauro@linares.com>
2020-01-26 18:43:30 -0300

    primera confirmación

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,29 @@
+class TodosController < ApplicationController
+
+  def new
+    @todo=Todo.new
+  end
+
+  def create
+    @todo=Todo.new(todo_params)
+    if @todo.save
+      flash[:notice]="The todo has been created successfully"
+      redirect_to @todo
+    else
+      render 'new'
+    end
+  end
+
+
+  def show
+    @todo=Todo.find(params[:id])
+  end
+
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:name, :description)
+  end
+
+end
